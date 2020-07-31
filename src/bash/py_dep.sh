88a5542720e4d58314347f5f9b6b4aa140e77312
Kirill Vercetti <office@kyzima-spb.com>
2020-07-31 22:08:46 +0300

    Python Dependency Manager Wrapper

---
+++ src/bash/py_dep.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+
+PIPFILES_PATH=${PIPFILES_PATH:-"Pipfile /Pipfile"}
+REQUIREMENTS_PATH=${REQUIREMENTS_PATH:-"requirements.txt /requirements.txt"}
+
+
+for path in $PIPFILES_PATH
+do
+    if [[ -f "$path" ]]
+    then
+        pushd $(dirname "$path")
+            echo "Found Pipfile. Installation of dependencies..."
+            pipenv install --system --deploy --ignore-pipfile
+        popd
+        exit 0
+    fi
+done
+
+
+for path in $REQUIREMENTS_PATH
+do
+    if [[ -f "$path" ]]
+    then
+        pushd $(dirname "$path")
+            echo "Found requirements.txt. Installation of dependencies..."
+            pip install --no-cache-dir --disable-pip-version-check -q -r "$path"
+        popd
+        exit 0
+    fi
+done
+
+
+if [[ -f "setup.py" ]]
+then
+    echo "Found setup.py. Installation this package as is..."
+    pip install --no-cache-dir --disable-pip-version-check -q .
+    exit 0
+fi
+
+
+echo "Not found package." >&2
+exit 1
