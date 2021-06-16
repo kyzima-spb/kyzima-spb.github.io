e310888fb7f297631443d107a8b6ecc48f4f9182
Kirill Vercetti <office@kyzima-spb.com>
2021-06-16 20:08:12 +0300

    Test

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_DIR=$(dirname "$(readlink -f "$0")")
+
+command pushd "$ROOT_DIR/src"
+    files=$(find . -type f | sed 's~^./~https://kyzima-spb.github.io~')
+    result=""
+
+    for url in $files; do
+      result+="<li><a href=\"$url\">$(basename "$url")</a></li>"
+    done
+
+    sed -r -e 's~<% links %>~'"$result"'~' "../index.tmpl" > "index.html"
+command popd
