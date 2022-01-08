c06bce9128621d907db804c531d6c2a9417cd09b
Kirill Vercetti <office@kyzima-spb.com>
2022-01-08 19:31:53 +0300

    fileEnv now work in sh

--- src/docker/secrets.sh
+++ src/docker/secrets.sh
@@ -1,26 +1,23 @@
-# usage: file_env VAR [DEFAULT]
-# ie: file_env 'XYZ_DB_PASSWORD' 'example'
-#     (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#     "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-# author: https://github.com/docker-library/mysql/blob/master/8.0/docker-entrypoint.sh
-file_env() {
+fileEnv() {
     local var="$1"
     local fileVar="${var}_FILE"
+    eval "local value=\$$var"
+    eval "local secretPath=\$$fileVar"
     local default="${2:-}"
     
-    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+  	if [[ "$value" ]] && [[ "$secretPath" ]]; then
         echo "Both $var and $fileVar are set (but are exclusive)" >&2
-        exit 1
+	      exit 1
+  	fi
+    
+    if [[ ! -z "$secretPath" ]]; then
+        value="$(cat "$secretPath")"
     fi
     
-    local value="$default"
-    
-    if [ "${!var:-}" ]; then
-        value="${!var}"
-    elif [ "${!fileVar:-}" ]; then
-        value="$(< "${!fileVar}")"
+    if [[ -z "$value" ]]; then
+        value="$default"
     fi
     
-    export "$var"="$value"
-    unset "$fileVar"
+  	export "$var"="$value"
+  	unset "$fileVar"
 }
