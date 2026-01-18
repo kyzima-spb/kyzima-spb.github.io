02f20b4c6d9997ca4ecd74f4a01f5a08343ea0cc
Kirill Vercetti <office@kyzima-spb.com>
2026-01-18 21:57:35 +0300

    Fix reset script

--- src/bash/jetbrains-reset.sh
+++ src/bash/jetbrains-reset.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
-declare -a apps=(
-    "DataGrip"
-    "CLion"
-    "Rider"
-    "WebStorm"
-    "GoLand"
-    "PyCharm"
-)
+apps=("CLion" "DataGrip" "GoLand" "IntelliJIdea" "PhpStorm" "PyCharm" "Rider" "WebStorm")
 
-for app in "${apps[@]}"; do
-    configDirs=$(find "$HOME/.config/JetBrains/" -iname "$app*" -type d)
 
-    for dir in $configDirs; do
+for app in "${apps[@]}"
+do
+    config_dirs=$(find "$HOME/.config/JetBrains/" -iname "$app*" -type d)
+
+    for dir in $config_dirs
+    do
         echo -n "Found: $(basename "$dir"). Clearing... "
+
         rm -rf "$dir/eval"
-        rm -rf "$dir/options/usage.statistics.xml"
-#        rm -rf "$dir/options/other.xml"
-#        rm -rf "$dir/options/recentProjects.xml"
-        rm -rf "$dir/options/updates.xml"
-        rm -rf "$dir/options/usage.statistics.xml"
+        # rm -rf "$dir/options/usage.statistics.xml"
+        rm -rf "$dir/options/other.xml"
+        # rm -rf "$dir/options/recentProjects.xml"
+        # rm -rf "$dir/options/updates.xml"
+
         echo "OK"
     done
 done
+
+rm -rf "$HOME/.java/.userPrefs/jetbrains" "$HOME/.java/.userPrefs/prefs.xml"
+
+exit 0
